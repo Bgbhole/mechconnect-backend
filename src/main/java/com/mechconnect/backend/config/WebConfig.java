d0d70930425346dd20441af98f5457ec022f16dc
Bgbhole <bgbhole3@gmail.com>
2026-07-12 15:06:55 +0530

    Update WebConfig.java

--- src/main/java/com/mechconnect/backend/config/WebConfig.java
+++ src/main/java/com/mechconnect/backend/config/WebConfig.java
@@ -1,34 +1,29 @@
-package com.mechconnect.backend.config;
+package com.ecommerce.ecommerce.config;
 
-/**
- * WebConfig
- *
- * Part of the MechConnect backend application.
- * Responsible for handling config related logic.
- */
-
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")  // all endpoints
-                        .allowedOrigins("http://127.0.0.1:5500") // your frontend
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-            
-          
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+            .allowedOrigins(
+    "https://bgbhole.github.io/E-commerce_frontend/"
+);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/");
     }
 }
